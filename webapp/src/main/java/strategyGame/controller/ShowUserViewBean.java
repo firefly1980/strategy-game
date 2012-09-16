a7555e9fd29c0a5d3aa59f5ffc0abe99b0deb31b
F.Troesch <f.troesch@t-online.de>
2012-09-16 19:40:59 +0200

    Added output of username.

---
+++ webapp/src/main/java/strategyGame/controller/ShowUserViewBean.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package strategyGame.controller;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * View Bean for Show User profile Page.
+ * 
+ * @author florian
+ */
+@ViewScoped
+@ManagedBean
+public class ShowUserViewBean implements Serializable {
+
+	private static final long serialVersionUID = 7169093813560495744L;
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ webapp/src/main/java/strategyGame/viewbeans/ShowUserProfileController.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package strategyGame.viewbeans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import strategyGame.controller.ShowUserViewBean;
+
+/**
+ * Controller for the showProfile Page.
+ * 
+ * @author florian
+ */
+@RequestScoped
+@ManagedBean
+public class ShowUserProfileController implements Serializable {
+
+	private static final long serialVersionUID = -341446467315131996L;
+
+	@ManagedProperty(value = "#{showUserViewBean}")
+	private ShowUserViewBean showUserViewBean;
+
+	public void init() {
+		if (!FacesContext.getCurrentInstance().isPostback())
+			showUserViewBean.setName("Ghosty");
+	}
+
+	public void doSomething() {
+
+	}
+
+	public ShowUserViewBean getShowUserViewBean() {
+		return showUserViewBean;
+	}
+
+	public void setShowUserViewBean(ShowUserViewBean showUserViewBean) {
+		this.showUserViewBean = showUserViewBean;
+	}
+}
